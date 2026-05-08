e4d41fdd67bee6342ade30315ca8bfb6c66da56f
AljoryAnas <445200074@student.ksu.edu.sa>
2026-05-08 19:17:14 +0300

    Refactor empty favourites table handling

--- user.php
+++ user.php
@@ -89,8 +89,7 @@ $(document).ready(function () {
         if (response === true) {
           link.closest("tr").remove();
         if ($("#favouritesTable tbody tr").length === 0) {
-          $("#favouritesTable").html("");
-          $("#favouritesTable").html("<p>No favourites added yet.</p>");
+          $("#favouritesTable").replaceWith("<p>No favourites added yet.</p>");
         }
         } else {
           alert("Could not remove recipe from favourites.");
