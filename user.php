031e196e3887a4c5c89a235dc6d94154095cab4e
Aljory Anas <445200074@student.ksu.edu.sa>
2026-05-08 19:16:18 +0300

    Clear favourites table when no favourites are present

--- user.php
+++ user.php
@@ -89,6 +89,7 @@ $(document).ready(function () {
         if (response === true) {
           link.closest("tr").remove();
         if ($("#favouritesTable tbody tr").length === 0) {
+          $("#favouritesTable").html("");
           $("#favouritesTable").html("<p>No favourites added yet.</p>");
         }
         } else {
