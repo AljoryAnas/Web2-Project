51ffc40ce6972c37149db68360d4ad0e555ff7d3
Aljory Anas <445200074@student.ksu.edu.sa>
2026-05-14 13:12:51 +0300

    Remove conflicting database connection line and clean up code

--- db.php
+++ db.php
@@ -4,12 +4,7 @@ $username = "root";
 $password = "root";
 $database = "recipes_project";
 
-<<<<<<< HEAD
-$conn = new mysqli($servername, $username, $password, $database, '8889');
-=======
 $conn = new mysqli($servername, $username, $password, $database);
-//$conn = new mysqli("sql208.infinityfree.com", "if0_41867996", "AKLTM445", "if0_41867996_recipes_project");
->>>>>>> f83b6bb963500788ddc4b70e754d508f41a50cfd
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
