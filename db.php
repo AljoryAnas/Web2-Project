1fff0b780b69b549fa559994bb390d69983044a9
Lama <mememomo-m77@hotmail.com>
2026-05-15 19:44:40 +0300

    Update database connection details in db.php

--- db.php
+++ db.php
@@ -1,8 +1,12 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "root";
-$database = "recipes_project";
+//$servername = "localhost";
+//$username = "root";
+//$password = "root";
+//$database = "recipes_project";
+$servername = "sql208.infinityfree.com";
+$username = "if0_41867996";
+$password = "AKLTM445";
+$database = "if0_41867996_recipes_project";
 
 $conn = new mysqli($servername, $username, $password, $database);
 
